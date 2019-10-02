af0ad3f7843e8c33e0367f04f162c2c3206dbf14
RupeshBista <Rbista@talentpath.com>
2019-10-02 15:05:16 -0500

    first commit

---
+++ assets/js/hangman.js
@@ -0,0 +1,13 @@
+// creat an array of the words.
+const word = ['fat','lazy','hungry'];
+
+// choose word randomly.
+let randNum = Math.floor(Math.random() * word.length);
+let choosenWord = word[randNum];
+console.log(choosenWord);
+// create underscores base on the length.
+
+// get user guess
+// check if guess is right 
+//if right push to right array
+// if wrong push to wrong array
